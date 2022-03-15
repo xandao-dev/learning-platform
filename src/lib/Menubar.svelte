a5d19120123066d7a9e2e6636aca63dbf4aaa6ad
Alexandre Calil <alexandrecalilmf@gmail.com>
2022-03-15 01:43:17 -0300

    feat: add menubar

--- src/lib/Menubar.svelte
+++ src/lib/Menubar.svelte
@@ -1,1 +1,21 @@
-<h1>Menu</h1>
+<script lang="ts">
+	import ProfileMenu from './ProfileMenu.svelte';
+</script>
+
+<div class="menubar">
+	<div />
+	<div />
+	<div>
+		<ProfileMenu />
+	</div>
+</div>
+
+<style>
+	.menubar {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		background-color: var(--color-secondary-light);
+		height: 4.5rem;
+	}
+</style>
---
+++ src/lib/ProfileMenu.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	let user = {
+		name: 'Alexandre Calil',
+	};
+</script>
+
+<div class="profile">
+	<div class="info">
+		<h1 class="name">{user.name}</h1>
+		<span class="action">Key Account</span>
+	</div>
+	<img
+		class="avatar"
+		src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200"
+		alt="user profile"
+	/>
+</div>
+
+<style>
+	.profile {
+		display: flex;
+		align-items: center;
+	}
+	.info {
+		margin-right: 1rem;
+		text-align: end;
+	}
+	.name {
+		font-size: 1rem;
+		font-weight: 400;
+		line-height: 1.375rem;
+		margin: 0;
+		color: var(--color-text-white);
+	}
+	.action {
+		font-size: 0.75rem;
+		line-height: 1rem;
+		color: var(--color-text-white);
+	}
+	.avatar {
+		width: 3.75rem;
+		height: 3.75rem;
+		border-radius: 50%;
+		margin-right: 2rem;
+	}
+</style>
